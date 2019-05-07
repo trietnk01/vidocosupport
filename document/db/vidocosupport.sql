2a5e2ecddedcb26c11ab4db33b937477ae72e201
dienkim <trietnk01@gmail.com>
2019-05-07 23:40:19 +0700

    cập nhật

--- document/db/vidocosupport.sql
+++ document/db/vidocosupport.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2019 at 06:35 PM
+-- Generation Time: May 07, 2019 at 06:37 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.1.26
 
